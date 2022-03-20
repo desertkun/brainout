edd2bd82fe36f2cff5a5ebcdcf7137b7aac3e3cd
desertkun <desertkun@gmail.com>
2022-03-20 20:45:15 +0200

    Correct version is valpha2

--- core/src/com/desertkun/brainout/Version.java
+++ core/src/com/desertkun/brainout/Version.java
@@ -2,6 +2,6 @@ package com.desertkun.brainout;
 
 public class Version
 {
-    public static final String VERSION = "valpha";
+    public static final String VERSION = "valpha2";
     public static final String TAG = "master";
 }
