32898d843aa93166bee0b9bfaa05ccbb59da5986
salva <atlas200x@gmail.com>
2022-07-08 14:36:56 -0500

    Revert "add: MyBoom command for testing"
    This reverts commit 2fe60985be963dd384068260018ef07112c35419.

--- server/src/com/desertkun/brainout/server/console/Console.java
+++ server/src/com/desertkun/brainout/server/console/Console.java
@@ -56,7 +56,6 @@ public class Console
         registerCommand("completequest", CompleteQuest.class);
         registerCommand("skipwarmup", SkipWarmUp.class);
         registerCommand("boom", Boom.class);
-        registerCommand("myboom", MyBoom.class);
         registerCommand("migrate", Migrate.class);
         registerCommand("renamedimension", RenameDimension.class);
         registerCommand("follow", FollowPlayer.class);
